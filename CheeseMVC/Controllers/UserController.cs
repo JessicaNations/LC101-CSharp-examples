63bcd4f44c23b0c8f53cb481a5329b660a9b3132
dwmoeller1 <dwmoeller1@aol.com>
2018-06-02 16:02:34 -0500

    In class code for user signup

---
+++ CheeseMVC/Controllers/UserController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Models.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index(string message)
+        {
+            ViewBag.message = message;
+            List<User> model = UserData.GetAll();
+            return View();
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            UserData.GetById(id);
+            return View();
+        }
+
+        // GET: User/Create
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(User user, string verify)
+        {
+            if (user.Password != verify)
+            {
+                ViewBag.message = "Passwords do not match";
+                user.Password = string.Empty;
+                return View(user);
+            }
+
+            UserData.Add(user);
+            string messageString = $"Welcome {user.UserName}!";
+            //return Redirect($"Index?message={messageString}");
+            return RedirectToAction("Index", new { message = messageString });
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CheeseMVC/Models/User/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheeseMVC.Models.User
+{
+    public class User
+    {
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+        [MaxLength(25)]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [MaxLength(25)]
+        public string Password { get; set; }
+    }
+}
